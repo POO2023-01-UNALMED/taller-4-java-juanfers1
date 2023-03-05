4c2feba5d3cba013bd21568eba1e74c64b8651df
Juan Fernando Castaño Durán <54716226+juanfers1@users.noreply.github.com>
2023-03-05 15:11:04 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,34 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10, "DISCIPLINAR"),FUNDAMENTACION(20,"FUNDAMENTACION"),ELECTIVA(30,"ELECTIVA");
+	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
-	int codigo;  //Quité el private
-	String nombre;
+	private int codigo;  
+	private String nombre;
+
+	private Tipo(int codigo) { //Nuevo constructor para el enum con un parámetro
+		this.codigo = codigo;
+	}
+
 	private Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+	
+	//Métodos get y set para los atributos de la clase Tipo
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 }
